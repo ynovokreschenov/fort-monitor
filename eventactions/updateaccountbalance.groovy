6aa6b65449cf2161712910a418028497c2d63d8d
Yuri Novokreschenov <yuri.novokreschenov@gmail.com>
2022-09-14 14:53:55 +0300

    updateaccountbalance added

---
+++ eventactions/updateaccountbalance.groovy
@@ -0,0 +1,22 @@
+def fmAccount = subject.fmAccount
+def fmAccountNumber = fmAccount.fmAccountNum
+def balance = subject.fmBalance
+def description = 'Добавление из платежа ITSM365'
+
+def sid = modules.fortmonitor.connect().result
+def accountInfo = modules.fortmonitor.httpRequest(
+  method='get',
+  path='updateaccountbalance',
+  query=[
+    'accountNumber':fmAccountNumber,
+    'balance':balance,
+    'description':description
+    ],
+  headers=[
+    'SessionId':sid
+    ]
+  ).result
+
+modules.fortmonitor.disconnect()
+
+utils.event(subject, accountInfo)
